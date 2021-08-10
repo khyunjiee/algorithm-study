52e9e57e76f612eb6225b7df21502b74b382def3
이정수 <jeongsoo.lee7420@gmail.com>
2021-08-10 23:37:30 +0900

    ADD 숨바꼭질 1697

--- src/이정수/week3/숨바꼭질_1697.java
+++ src/이정수/week3/숨바꼭질_1697.java
@@ -13,13 +13,13 @@ public class 숨바꼭질_1697 {
 	 * 
 	 * 메모리초과 문제 발생 
 	 * ->  bfs로 푸는 것은 맞고 bfs에서 큐는 필수 입니다. 문제가 되는 부분은
-	 * 갔던 곳을 또가는 문제이므로 이를 메모해서 걸러주면 됩니다. 메모는 맵으로 하여 메모리낭비와 접근 
-	 * 오버헤드를 줄입니다.
-	 * -> 그래도 안되네요 좌표가 음수가 되는 경우도 배제 해야겠습니다.
-	 * 
+	 * 갔던 곳을 또가는 문제이므로 이를 메모해서 걸러주면 됩니다. 
+	 * -> 그래도 안되네요 좌표가 음수가 되는 경우, 최대값 좌표를 넘어가는 경우도 배제 해야겠습니다.
+	 * -> 해결
 	 * 
 	 * 시간복잡도: 알 수 없음 -> 고려하지 않아도 알아서 인풋값 적당히 주는것으로 간주 가능
 	 */
+	static boolean[] visited = new boolean[100001];
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -33,27 +33,23 @@ public class 숨바꼭질_1697 {
 	public static int bfs(int N, int K) {
 		Map<Integer, Boolean> map = new HashMap<>();
 		Queue<Integer> queue = new LinkedList<>();
-		int time=0;
-		int size;
-		
+		int time=0, size = 0, current;
 		// 첫번째 위치
 		queue.offer(N);
-		int newN;
+		
 		while(true) {
 			size = queue.size();
 			while(--size>=0) {
 				// 방문
-				newN = queue.poll();
-				// 해당 좌표 방문여부 확인 후 메모
-//				if(map.containsKey(newN)) continue;
-//				map.put(newN, true);
+				current = queue.poll();
+				if(current<0||current>100000||visited[current]) continue;
 				
-				if(newN<0|newN>100100) continue;
+				visited[current] = true;
+				if(current == K) return time;
 				
-				if(newN == K) return time;
-				queue.offer(newN-1);
-				queue.offer(newN+1);
-				queue.offer(newN*2);
+				queue.offer(current-1);
+				queue.offer(current+1);
+				queue.offer(current*2);
 			}
 			time++; // 시간 증가
 		}
