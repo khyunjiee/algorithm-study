0b778b2b7ef07cd3fad0417f65ff64117009f922
이정수 <jeongsoo.lee7420@gmail.com>
2021-11-12 17:12:35 +0900

    UDATE 기둥과보설치

--- src/이정수/week14/기둥과보설치_60061.java
+++ src/이정수/week14/기둥과보설치_60061.java
@@ -7,119 +7,112 @@ import java.util.Map;
 
 public class 기둥과보설치_60061 {
 	/*
-	 * 접근:
-	 * 못풀었습니다
-	 * 처음에 풀다 실패해서 답안을 봤는데도
-	 * 디버깅이 안되네요
+	 * 접근: 1. 기둥과 보를 세울 수 있는지 여부 확인하는 메소드 생성 
+	 * 2. 삭제시 해당 기둥이나 보를 삭제하고 나머지 구조물들에 대해
+	 * 가능여부 확인 
+	 * 3. 맵에 패딩줘서 indexOutOfBounds Exception방지
 	 * 
 	 * 시간복잡도:
-	 * 
+	 * ?
 	 * 
 	 * 풀이에 걸린 시간:
-	 * 5h..
+	 * 7h
 	 * 
 	 */
 	public static void main(String[] args) {
-		System.out.println(Arrays.deepToString(solution(5,new int[][] {{0,0,0,1},{2,0,0,1},{4,0,0,1},{0,1,1,1},{1,1,1,1},{2,1,1,1},{3,1,1,1},{2,0,0,0},{1,1,1,0},{2,2,0,1}})));
+		System.out.println(Arrays.deepToString(solution(5,
+				new int[][] { { 0, 0, 0, 1 }, { 2, 0, 0, 1 }, { 4, 0, 0, 1 }, { 0, 1, 1, 1 }, { 1, 1, 1, 1 },
+						{ 2, 1, 1, 1 }, { 3, 1, 1, 1 }, { 2, 0, 0, 0 }, { 1, 1, 1, 0 }, { 2, 2, 0, 1 } })));
 	}
-	
-	
-	static final int PILLAR=0, BEAM=1, INSTALL=1, REMOVE=0;
+
+	static final int PILLAR = 0, BEAM = 1, INSTALL = 1, REMOVE = 0;
 	static int N;
-	static Map<Integer, int[]> answerMap;
-	static boolean[][] pillar,beam;
+	static boolean[][] pillar, beam;
+
 	public static int[][] solution(int n, int[][] build_frame) {
-		answerMap = new HashMap<>();
-		N = n;
-		pillar = new boolean[n+3][n+3];
-		beam = new boolean[n+3][n+3];
-		
-		for (int i = 0; i < build_frame.length; i++) {
-			int x = build_frame[i][0]+1;
-			int y = build_frame[i][1]+1;
-			int a = build_frame[i][2];
-			int b = build_frame[i][3];
-			
-			if(b==INSTALL) { // 설치
-				if(a==PILLAR) { // 기둥
-					if(canInstallPillar(x,y)) 
-						install(x,y,a);
-				}else if(a==BEAM) { // 설치
-					if(canInstallBeam(x,y))
-						install(x,y,a);
+		int frameCount = 0;
+		N = n + 3;
+		pillar = new boolean[N][N];
+		beam = new boolean[N][N];
+
+		for (int[] frame : build_frame) {
+			int x = frame[0] + 1;
+			int y = frame[1] + 1;
+			int frameType = frame[2];
+			int installOrRemove = frame[3];
+
+			if (installOrRemove == INSTALL) {
+				if (frameType == PILLAR) {
+					if (canInstallPillar(x, y)) {
+						pillar[x][y] = true;
+						frameCount++;
+					}
+				} else if (frameType == BEAM) {
+					if (canInstallBeam(x, y)) {
+						beam[x][y] = true;
+						frameCount++;
+					}
 				}
-			}else if(b==REMOVE) { // 삭제
-				if(a==PILLAR) pillar[y][x] = false;
-				else if(a==BEAM) beam[y][x] = false;
-				
-				if(canRemove()) {
-					remove(x, y, a);
-				}else {
-					if(a==PILLAR) pillar[y][x] = true;
-					else if(a==BEAM) beam[y][x] = true;
+			} else if (installOrRemove == REMOVE) {
+				// 프레임 제거
+				if (frameType == PILLAR) {
+					pillar[x][y] = false;
+				} else if (frameType == BEAM) {
+					beam[x][y] = false;
 				}
+
+				if (canRemove(x, y, frameType)) {
+					frameCount--;
+					continue;
+				}
+
+				// 프레임 복구
+				if (frameType == PILLAR) {
+					pillar[x][y] = true;
+				} else if (frameType == BEAM) {
+					beam[x][y] = true;
+				}
+			}
+
+		}
+
+		int[][] answer = new int[frameCount][];
+		int idx = 0;
+
+		for (int i = 1; i < N - 1; i++) {
+			for (int j = 1; j < N - 1; j++) {
+				if (pillar[i][j])
+					answer[idx++] = new int[] { i - 1, j - 1, PILLAR };
+				if (beam[i][j])
+					answer[idx++] = new int[] { i - 1, j - 1, BEAM };
 			}
 		}
-		
-		// Map -> Array
-        int[][] answer = answerMap.values().toArray(new int[0][]);
-        
-        // 정렬
-        Arrays.sort(answer, new Comparator<int[]>(){
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if(o1[0]==o2[0] && o1[1]==o2[1]) {
-					return o1[2] - o2[2];
-				}else if(o1[0]==o2[0]) {
-					return o1[1]-o2[1];
-				}
-				return o1[0]-o2[0];
-			}
-		});
-        
-        return answer;
-    }
-	
-	
-	private static boolean canRemove() {
-		for (int i = 1; i < N+2; i++) {
-			for (int j = 1; j < N+2; j++) {
-				if(pillar[j][i] && !canInstallPillar(i,j))
+
+		return answer;
+	}
+
+	// 프레임 제거 후 나머지 프레임 조건 만족 여부 확인 메소드
+	private static boolean canRemove(int x, int y, int frameType) {
+		for (int i = 1; i < N - 1; i++) {
+			for (int j = 1; j < N - 1; j++) {
+				if (pillar[i][j] && !canInstallPillar(i, j))
 					return false;
-				if(beam[j][i] && !canInstallBeam(i, j))
+				if (beam[i][j] && !canInstallBeam(i, j))
 					return false;
 			}
 		}
+
 		return true;
 	}
 
-
-	// 설치
-	private static void install(int x, int y, int a) {
-		if(a==PILLAR) pillar[y][x] = true;
-		else beam[y][x] = true;
-		answerMap.put(x*(N+3)+y, new int[] {x-1,y-1,a});
-	}
-	
-	// 삭제
-	private static void remove(int x, int y, int a) {
-		if(a==PILLAR) pillar[y][x] = false;
-		else beam[y][x] = false;
-		answerMap.remove(x*(N+3)+y);
-	}
-
-	// 기둥 설치 가능 여부 확인
-	private static boolean canInstallPillar(int x, int y) {
-		// 바닥 위에 있거나 보의 한쪽 끝 부분 위에 있거나, 또는 다른 기둥 위에 있거나
-		return y==1 || beam[y][x-1] || beam[y][x] || pillar[y-1][x];
-		
-	}
-	
-	// 보 설치 가능 여부 확인
 	private static boolean canInstallBeam(int x, int y) {
-		// 한쪽 끝 부분이 기둥 위에 있거나, 또는 양쪽 끝 부분이 다른 보와 동시에 연결
-		return pillar[y-1][x] || pillar[y-1][x+1] || (beam[y][x-1] && beam[y][x+1]);
+		// 한쪽 끝 부분이 기둥 위에 있거나 || 양쪽 끝 부분이 다른 보와 동시에 연결
+		return pillar[x][y - 1] || pillar[x + 1][y - 1] || (beam[x - 1][y] && beam[x + 1][y]);
 	}
-	
-	
+
+	private static boolean canInstallPillar(int x, int y) {
+		// 바닥 위에 있거나 || 보의 한쪽 끝 부분 위에 있거나 || 다른 기둥 위에 있거나
+		return y == 1 || beam[x][y] || beam[x - 1][y] || pillar[x][y - 1];
+	}
+
 }
